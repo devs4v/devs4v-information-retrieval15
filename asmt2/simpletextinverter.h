166aaa8a891cf5f83dd124234c667725bef1a318
Shivam Chaturvedi <devs4v@gmail.com>
2015-02-12 01:24:22 +0530

    Added missing set import removed lexicography sorter struct as set implicitly does so. Added filename parameter to write_tokens_to_file function. Removed constructor and destructor functions. modified emit definition to accept directory param as char* as direc, converts to string directory. Reverted to set<string> everywhere and removed the lexico ordering struct refs. fixed regex bug. Added missing scope to _split function.
    Ran code and checked that it works perfectly. Emit file created.

--- asmt2/simpletextinverter.h
+++ asmt2/simpletextinverter.h
@@ -3,6 +3,7 @@
 #include <string>
 #include <vector>
 #include <map>
+#include <set>
 #include <regex>
 #include <iterator>
 #include <utility>
@@ -13,75 +14,73 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 
+#define d true
 
 using namespace std;
 
-struct _sort_by_lexicography {
-    static bool compare_chars(char x, char y) {
-        return toupper(x) < toupper(y);
-    }
-    bool operator()(const string& x, const string& y) const {
-        return lexicographical_compare(x.begin(), x.end(), y.begin(), y.end(), compare_chars);
-    }
-};
+// struct _sort_by_lexicography {
+//     static bool compare_chars(char x, char y) {
+//         return toupper(x) < toupper(y);
+//     }
+//     bool operator()(const string& x, const string& y) const {
+//         return lexicographical_compare(x.begin(), x.end(), y.begin(), y.end(), compare_chars);
+//     }
+// };
 class SimpleTextInverter{
     private:
         // map<string, string> _map_string_data_to_tokens(const string);
         int _parse_file_to_tokens(const string, set<string>&);
         int _get_file_data(const string, string&);
-        int _write_tokens_to_file(set<string>&);
+        int _write_tokens_to_file(set<string>&, string);
         int _remove_escape_chars(string&);
         set<string> _tokenize(string&);
-        set<string> _split(string&, string){
+        set<string> _split(string&, string);
     public:
-        SimpleTextInverter();
         void emit(char *);
         void sort(char *);
         void invert(char *);
-        ~SimpleTextInverter();
         
 };
 
-void SimpleTextInverter::emit(string directory){
+void SimpleTextInverter::emit(char *direc){
     
     ifstream fin;
-    
+    string directory(direc);
 
-    string dir, filepath;
-    string data;
+    string filepath;
     DIR *dp;
     struct dirent *dirp;
     struct stat filestat;
     
     string fileContents;
-    set<string, _sort_by_lexicography> mapped_tokens;
+    set<string> mapped_tokens;
     string filename;
     errno = 0;
-    dp = opendir( dir.c_str() );
+    dp = opendir( directory.c_str() );
     if(dp == NULL){
-        cout << "Error(" << errno << ") opening " << dir << endl;
-        return errno;
+        cout << "Error(" << errno << ") opening " << directory << endl;
+        return;
     }
 
     while ((dirp = readdir( dp ))){
-        filepath = dir + "/" + dirp->d_name;
+        filepath = directory + "/" + dirp->d_name;
         filename = dirp->d_name;
-        if(d)cout<<"\nreading"<<filepath;
+        if(d)cout<<"\nreading "<<filepath;
 
         // If the file is a directory (or is in some way invalid) we'll skip it 
         if (stat( filepath.c_str(), &filestat )) continue;
         if (S_ISDIR( filestat.st_mode ))         continue;
 
-        
-            if(0 != _parse_file_to_tokens(filepath, mapped_tokens)){
-                cout<<"\nError: Error in parsing file for tokens. (Continuing with next file.)";
-                continue;
-            }
+        cout<<"\nParsing "<<filename;
+        if(0 != _parse_file_to_tokens(filepath, mapped_tokens)){
+            cout<<"\nError: Error in parsing file for tokens. (Continuing with next file.)";
+            continue;
+        }
 
-            if(0 != _write_tokens_to_file(mapped_tokens)){
-                cout<<"\nError: Error in writing tokens to disk. (Continuing with next file.)";
-                continue;
-            }
+        if(0 != _write_tokens_to_file(mapped_tokens, filename)){
+            cout<<"\nError: Error in writing tokens to disk. (Continuing with next file.)";
+            continue;
+        }
             
 
         if(d)cout<<"\nFinished writing to emit for "<<filename;
@@ -111,7 +110,7 @@ int SimpleTextInverter::_parse_file_to_tokens(const string filename, set<string>
         cout<<"\nError: Error in removing escape chars.";
         return -1;
     }
-    tokens = tokenize(sanitized_chars);
+    tokens = _tokenize(fileData);
 
     return 0;
 }
@@ -137,7 +136,7 @@ int SimpleTextInverter::_remove_escape_chars(string& str){
     string blankie = " ";
     string result;
     // regex no_tag_regex("<.*>");
-    regex no_special_chars("[(\\n)|(\\t)|(\\/)|(\\\\)|(*)|(,)|(.)|(;)|(:)|(!)|(?)|(')|(\")|([)|(])|(()|())]");
+    regex no_special_chars("[(\\n)|(\\t)|(\\/)|(\\*)|(,)|(\\.)|(;)|(:)|(\\!)|(\\?)|(')|(\")|(\\[)|(\\])|(\\()|(\\))]");
     // regex ("[]");
 
     // result = regex_replace(doc, no_tag_regex, blankie);
@@ -147,11 +146,11 @@ int SimpleTextInverter::_remove_escape_chars(string& str){
 }
 
 set<string> SimpleTextInverter::_tokenize(string& no_tag_doc){
-    return split(no_tag_doc, " ");
+    return _split(no_tag_doc, " ");
 }
 
-set<string> _split(string& s, string delim){
-    set<string, _sort_by_lexicography> res;
+set<string> SimpleTextInverter::_split(string& s, string delim){
+    set<string> res;
     
     if(delim.empty()){
         // vector<string> res;
@@ -171,12 +170,12 @@ set<string> _split(string& s, string delim){
     return res;
 }
 
-int SimpleTextInverter::_write_tokens_to_file(set<string>& tokens){
+int SimpleTextInverter::_write_tokens_to_file(set<string>& tokens, string filename){
     ofstream fout;
     fout.open("emit.out", ios::out | ios::app);
 
     if(fout.is_open()){
-        for (set<string>::iterator i = mapped_tokens.begin(); i != mapped_tokens.end(); ++i){
+        for (set<string>::iterator i = tokens.begin(); i != tokens.end(); ++i){
             fout<<*i<<":"<<filename<<endl;
         }
     }else{

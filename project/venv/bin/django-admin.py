df91d434c907888215fb1e12b4bf11ef7fca3e8a
Shivam Chaturvedi <devs4v@gmail.com>
2015-04-13 23:25:29 +0530

    Added django

---
+++ project/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/shivam/projects/devs4v-information-retrieval15/project/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()

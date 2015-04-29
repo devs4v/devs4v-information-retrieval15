acab25e6d33ca8880eff3462ca27129cd2a55b47
Shivam Chaturvedi <devs4v@gmail.com>
2015-04-29 15:13:23 +0530

    Added statistic printing and corrected SO_QA and added YM_QA.

--- project/nlqa/search/YahooManners_QA.py
+++ project/nlqa/search/YahooManners_QA.py
@@ -10,25 +10,25 @@ from org.apache.lucene.util import Version
 
 
 
-class StackOverflow_QA():
-	posts_cols = ['Id', 'PostTypeId', 'ParentId', 'AcceptedAnswerId', 'CreationDate', 'Body', 'OwnerUserId', 'Title', 'Tags', 'Topic',]
-	comments_cols = ['id', 'postid', 'score', 'text', 'date', 'userid', 'topic',]
+class YahooManners_QA():
+	posts_cols = ['uri', 'subject', 'content', 'cat', 'maincat', 'subcat', 'yid', 'bestyid',]
+	# comments_cols = ['id', 'postid', 'score', 'text', 'date', 'userid', 'topic',]
 
 	_posts_fields = ['id', 'title', 'question', 'tag', 'extra',]
 	_answer_fields = ['answer_id', 'answer']
-	_comment_fields = ['comment_id', 'comment']
+	# _comment_fields = ['comment_id', 'comment']
 	_cols_to_fields = {
-						'id'		: 'Id',
-						'title'		: 'Title',
-						'question'	: 'Body',
-						'tag'		: 'Tags',
-						'extra'		: 'Topic',
+						'id'		: 'uri',
+						'title'		: 'subject',
+						'question'	: 'content',
+						'tag'		: 'maincat',
+						'extra'		: 'subcat',
 
-						'answer_id'	: 'Id',
-						'answer'	: 'Body',
+						'answer_id'	: 'uri',
+						'answer'	: 'answer',
 						
-						'comment_id': 'id',
-						'comment'	: 'text',
+						# 'comment_id': 'id',
+						# 'comment'	: 'text',
 	}
 
 	def __init__(self, conn_string=None, index_dir = None, debug=True):
@@ -43,19 +43,22 @@ class StackOverflow_QA():
 		self.index_dir = index_dir
 
 		self.sqls = {
-				'all_question_ids' 	: """ SELECT "Id" FROM so_posts ORDER BY "Id" """,
-				'questions' 		: """ SELECT "Id", "Title", "Body", "Tags", "Topic" FROM so_posts WHERE "ParentId" = -1 ORDER BY "Id", "Topic" """,
-				'question_by_id' 	: """ SELECT "Id", "Title", "Body", "Tags", "Topic" FROM so_posts WHERE "Id" = {question_id} ORDER BY "Id" """,
-				'answers'			: """ SELECT "Id", "Body" FROM so_posts WHERE "Id" = {question_id} ORDER BY "Id" """,
-				'comments'			: """ SELECT id, text FROM so_comments WHERE postid = {post_id} """
+				'all_question_ids' 	: """ SELECT uri FROM ym_posts ORDER BY uri """,
+				'questions' 		: """ SELECT uri, subject, content, maincat, subcat FROM ym_posts ORDER BY uri """,
+				'question_by_id' 	: """ SELECT uri, subject, content, maincat, subcat FROM ym_posts WHERE uri = {question_id} ORDER BY uri """,
+				'answers'			: """ SELECT uri, answer FROM ym_posts WHERE uri = {question_id} ORDER BY uri """,
+				# 'comments'			: """ SELECT id, text FROM so_comments WHERE postid = {post_id} """
 			}
+
+		self.doctype = "ym"
+		
 		self.debug = debug
 		self.verbose_values = False
 		if self.debug:
 			for k in self.sqls.keys():
 				self.sqls[k] = self.sqls[k] + " LIMIT 10"
 
-		if self.debug: print "StackOverflow_QA indexer ver 0.1"
+		if self.debug: print "YahooManners_QA indexer ver 0.1"
 		
 	def _check_database_connection(self):
 		if self.connection_string is None or self.connection_string.strip() == "":
@@ -107,7 +110,7 @@ class StackOverflow_QA():
 						'title'		: indexedField,
 						'question'	: indexedField,
 						'answer'	: indexedField,
-						'comment'	: indexedField,
+						# 'comment'	: indexedField,
 						'tag'		: indexedField,
 						'extra'		: indexedField,
 		}
@@ -126,7 +129,7 @@ class StackOverflow_QA():
 				if self.debug: print "+"*10, "\nMaking new Document"
 				doc = Document()
 				if self.debug: print "Adding doc type"
-				doc.add(Field("type", 'so', fieldTypes['type']))
+				doc.add(Field("type", self.doctype, fieldTypes['type']))
 				
 				# make fields
 				if self.debug: print "Adding post fields"
